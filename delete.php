6dff1011d463ec827c3d4d8c6d1279f39fe45307
it63070113 <73680313+it63070113@users.noreply.github.com>
2020-11-13 23:38:57 +0700

    Add files via upload

---
+++ delete.php
@@ -0,0 +1,20 @@
+<?php
+
+$conn = mysqli_init();
+mysqli_real_connect($conn, 'dataif.mysql.database.azure.com', 'it63070113@dataif', 'EMLcnk22', 'itflab', 3306);
+
+$id = $_GET['id']; // get id through query string
+
+$del = mysqli_query($db,"delete from tblemp where id = '$id'"); // delete query
+
+if($del)
+{
+    mysqli_close($db); // Close connection
+    header("location:show.php"); // redirects to all records page
+    exit;	
+}
+else
+{
+    echo "Error deleting record"; // display error message if not delete
+}
+?>
