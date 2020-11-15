17160d15d345ecc2e45d8a6832687b23f38ebbcd
it63070113 <73680313+it63070113@users.noreply.github.com>
2020-11-15 21:10:12 +0700

    Update delete.php

--- delete.php
+++ delete.php
@@ -1,20 +1,20 @@
 <?php
 
 $conn = mysqli_init();
-mysqli_real_connect($conn, 'dataif.mysql.database.azure.com', 'it63070113@dataif', 'EMLcnk22', 'itflab', 3306);
+mysqli_real_connect($conn,'dataif.mysql.database.azure.com', 'it63070113@dataif', 'EMLcnk22', 'itflab', 3306);
 
-$id = $_GET['id']; // get id through query string
+$id = $_GET['id','link','comment','name'];
 
-$del = mysqli_query($conn,'DELETE * FROM guestbook= "$id"'); // delete query
+$del = mysqli_query($conn,'DELETE * FROM guestbook= '$id,$link,$comment,$name'');
 
 if($del)
 {
-    mysqli_close($conn); // Close connection
-    header("location:show.php"); // redirects to all records page
-    exit;	
+    mysqli_close($conn);
+    header("location:show.php");
+    exit;
 }
 else
 {
-    echo "Error deleting record"; // display error message if not delete
+    echo "Error deleting record";
 }
 ?>
